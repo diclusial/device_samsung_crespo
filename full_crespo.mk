5217a8f30635a8f8e7dd2267ce8237a0627071c9
diclusial <diclusial@server.fake>
2012-12-01 22:26:02 -0800

    Set the build prop value for version, brand, etc.

--- full_crespo.mk
+++ full_crespo.mk
@@ -38,5 +38,14 @@ $(call inherit-product, device/samsung/crespo/device.mk)
 # Discard inherited values and use our own instead.
 PRODUCT_NAME := full_crespo
 PRODUCT_DEVICE := crespo
-PRODUCT_BRAND := Android
-PRODUCT_MODEL := Full Android on Crespo
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Nexus S
+PRODUCT_MANUFACTURER := Samsung
+PLATFORM_VERSION := 4.2.1
+
+# Use build.prop values similar to Google's values
+PRODUCT_BUILD_PROP_OVERRIDES := \
+    PRODUCT_NAME=soju \
+    BUILD_FINGERPRINT="google/soju/crespo:4.2.1/JOP40D/533553:user/release-keys" \
+    PRIVATE_BUILD_DESC="soju-user 4.2.1 JOP40D 533553 release-keys" \
+    BUILD_NUMBER=533553
