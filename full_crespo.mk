80d7390b1416c529068a6cb41856ceb2e5661aca
diclusial <diclusial@server.fake>
2012-12-01 22:26:52 -0800

    Remove redundant PRODUCT_PACKAGES for live wallpapers

--- full_crespo.mk
+++ full_crespo.mk
@@ -22,13 +22,6 @@
 # Get the long list of APNs
 PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
 
-# Live Wallpapers
-PRODUCT_PACKAGES += \
-        LiveWallpapers \
-        LiveWallpapersPicker \
-        VisualizationWallpapers \
-        librs_jni
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # This is where we'd set a backup provider if we had one
