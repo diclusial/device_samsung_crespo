95c0913777ddadeb072ed1d53511dde5322fba60
diclusial <diclusial@server.fake>
2012-12-02 16:40:37 -0800

    Add bluetooth build cfg

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,6 +23,7 @@
 # Use the open-source board-info file if none was set by the vendor
 # config.
 TARGET_BOARD_INFO_FILE ?= device/samsung/crespo/board-info.txt
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/crespo/bluetooth
 
 # Use the parts that are common between all crespos
 include device/samsung/crespo/BoardConfigCommon.mk
